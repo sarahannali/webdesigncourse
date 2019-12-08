9b81c6b966cd44a29f06ddb0de21ee17916f00ae
Sarah Ali <asarahali00@gmail.com>
2019-12-08 16:39:24 -0600

    added practice website and javascript beginning

---
+++ JavaScript_Practice/practice.js
@@ -0,0 +1,9 @@
+var firstName = prompt("what is your first name?")
+var lastName = prompt("what is your last name?")
+var age = prompt("what is your age?")
+console.log("Nice to meet you " + firstName + " " + lastName + "!")
+console.log("You are " + age + " years old :O")
+
+var daysAge = age*365.25
+
+console.log("That means you've been alive for " + daysAge + " days!!")
